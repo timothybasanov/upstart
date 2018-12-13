8e16a3e96a3e6fb50aa1a1415e531f241b43226b
Timothy Basanov <timofey.basanov@gmail.com>
2018-12-13 09:07:46 -0800

    Add JSP support

--- src/main/java/com/timothy/upstart/UpstartApplication.java
+++ src/main/java/com/timothy/upstart/UpstartApplication.java
@@ -2,12 +2,19 @@ package com.timothy.upstart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class UpstartApplication {
+public class UpstartApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(UpstartApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(UpstartApplication.class);
     }
 }
 
